5faa0fed47ec50b821b335c6b8771c45a3d69ed8
remvst <remvst@gmail.com>
2024-05-24 11:36:49 -0400

    Deps

--- src/entities/debug/bounds-view-controller.ts
+++ src/entities/debug/bounds-view-controller.ts
@@ -1,16 +1,17 @@
 import { InterpolationPool } from "@remvst/animate.js";
 import { Entity } from "@remvst/game-model";
-import { randPick } from "@remvst/random";
 import * as PIXI from "pixi.js";
 import { WorldViewController } from "../../world/world-view-controller";
 import { EntityViewController } from "../entity-view-controller";
 
+const COLORS = [
+    0xff0000, 0xffff00, 0x0000ff, 0xff00ff, 0x00ffff, 0x00ff00,
+];
+
 export class BoundsViewController extends EntityViewController<PIXI.Sprite> {
     private wrappedViewController: EntityViewController<PIXI.DisplayObject>;
 
-    private readonly color = randPick([
-        0xff0000, 0xffff00, 0x0000ff, 0xff00ff, 0x00ffff, 0x00ff00,
-    ]);
+    private readonly color = COLORS[Math.floor(Math.random() * COLORS.length)];
 
     constructor(
         wrappedViewController: EntityViewController<PIXI.DisplayObject>,
